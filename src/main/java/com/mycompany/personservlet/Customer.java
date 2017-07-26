24176362bd109df5e1dde9b47fee78904f6fe5d4
TomaszMacisowicz <TomaszMacisowicz@users.noreply.github.com>
2017-07-26 09:57:02 +0200

    add new files

---
+++ src/main/java/com/mycompany/personservlet/Customer.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personservlet;
+
+/**
+ *
+ * @author MAC
+ */
+public class Customer {
+
+    private String name;
+    private String password;
+    private GENDER gender;
+    private String hobby[];
+
+    public Customer() {
+
+    }
+
+    public Customer(String name, String password, GENDER gender, String[] hobby) {
+        this.name = name;
+        this.password = password;
+        this.gender = gender;
+        this.hobby = hobby;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public GENDER getGender() {
+        return gender;
+    }
+
+    public void setGender(GENDER gender) {
+        this.gender = gender;
+    }
+
+    public String[] getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String[] hobby) {
+        this.hobby = hobby;
+    }
+}
